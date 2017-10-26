6fc60f6ce3351b31a075b3e2d21d96b3e9b30940
XinZouS <sanking.saphira@gmail.com>
2017-10-26 18:03:12 -0400

    [ADD] log page header for Trips;

--- carryonex/OrdersShipperPageCell.swift
+++ carryonex/OrdersShipperPageCell.swift
@@ -12,20 +12,59 @@ import UIKit
 
 class OrdersShipperPageCell : OrdersSenderPageCell {
     
+    struct Pair {
+        let trip     : Trip
+        let requests : [Request]
+        init(_ trp: Trip, _ reqs: [Request]) {
+            self.requests = reqs
+            self.trip = trp
+        }
+    }
+    
+    var pairList = [Pair]()
+
+        
+    let cellIdOrderLogShipperHeader = "cellIdOrderLogShipperHeader"
     let cellIdOrderLogShipperCell = "cellIdOrderLogShipperCell"
     
     override func registerCollectionView(){
         collectionView.register(OrderLogShipperCell.self, forCellWithReuseIdentifier: cellIdOrderLogShipperCell)
+        collectionView.register(OrdersShipperHeaderCell.self, forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: cellIdOrderLogShipperHeader)
+    }
+    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return pairList.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if pairList.count == 0 {
+            return 0
+        }else{
+            return pairList[section].requests.count
+        }
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellIdOrderLogShipperCell, for: indexPath) as! OrderLogShipperCell
         
-        if indexPath.item < (dataList?.count)! {
-            cell.request = dataList?[indexPath.item]
+        let pair = pairList[indexPath.section]
+        for req in pair.requests {
+            cell.request = req
         }
-        
+
         return cell
     }
-
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        if kind == UICollectionElementKindSectionHeader {
+            let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: cellIdOrderLogShipperHeader, for: indexPath)
+            return header
+        }else{
+            return UICollectionReusableView()
+        }
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: collectionView.bounds.width, height: 40)
+    }
+    
 }
