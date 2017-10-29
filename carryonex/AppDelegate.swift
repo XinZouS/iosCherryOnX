87717b27f321851bd6bea432f415bd45e7d1dee9
Zian Chen <zian.chen@viacom.com>
2017-10-29 02:01:23 -0400

    New service manager to handle any types of customer service.

--- carryonex/AppDelegate.swift
+++ carryonex/AppDelegate.swift
@@ -41,22 +41,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, WXApiDelegate {
 
         // setup WeChatSDK
         WXApi.registerApp(WX_APPID, enableMTA: true)
-        //set up Udesk
-        let UdeskAppkey = "ca3d8ae00d9609f1f37515cb"
-        let isProduction:Bool = false;
-        let entity = JPUSHRegisterEntity()
-        let notificationTypes: UIUserNotificationType = [UIUserNotificationType.alert, UIUserNotificationType.badge, UIUserNotificationType.sound]
-        entity.types = Int(notificationTypes.rawValue)
-        JPUSHService.register(forRemoteNotificationConfig: entity, delegate: nil)
-        JPUSHService.setup(withOption: launchOptions, appKey:UdeskAppkey , channel: "AppStore", apsForProduction: isProduction)
-        JPUSHService.registrationIDCompletionHandler { (rescode , registrationId) in
-            if(rescode == 0){
-                UdeskManager.registerDeviceToken(registrationId)
-            }else{
-                print("registrationId获取失败:",rescode)
-            }
-        }
-        
+
         //UDesk Manager
         if ProfileManager.shared.isLoggedIn() {
             let domain = "carryonex.udesk.cn"
@@ -147,7 +132,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, WXApiDelegate {
     }
     
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        JPUSHService.registerDeviceToken(deviceToken)
+        UdeskManager.registerDeviceToken(deviceToken)
         UdeskManager.startUdeskPush()
     }
 
