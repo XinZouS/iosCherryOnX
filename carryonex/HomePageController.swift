80e8f7658fb9500aca5eb652afe2e6aaa0c5280f
XinZouS <sanking.saphira@gmail.com>
2017-11-03 14:04:22 -0400

    [FIX] use closure to handle displayAlert;

--- carryonex/HomePageController.swift
+++ carryonex/HomePageController.swift
@@ -199,7 +199,12 @@ class HomePageController: UIViewController, UISearchResultsUpdating,UICollection
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         UIApplication.shared.statusBarStyle = .default
-        ReachabilityManager.shared.startObserving(inViewController: self)
+        ReachabilityManager.shared.startObserving { (isConnecting) in
+            if !isConnecting {
+                let msg = "⚠️您的网络不可用，为了更准确即时地更新您的数据信息，请确保手机能使用WiFi或流量数据。对此给您带来的不便只好忍忍了，反正您也不能来打我。"
+                self.displayAlert(title: "无法链接到服务器", message: msg, action: "来人！给我拿下！")
+            }
+        }
     }
     
     override func viewDidAppear(_ animated: Bool) {
--- carryonex/Managers/ReachabilityManager.swift
+++ carryonex/Managers/ReachabilityManager.swift
@@ -15,9 +15,12 @@ class ReachabilityManager {
     
     let reachability = Reachability()!
     
-    fileprivate var targetViewController: UIViewController? // for presenting alert
-    
     private init(){
+        do{
+            try self.reachability.startNotifier()
+        }catch{
+            print("error in ReachabilityManager::startObserving, could not start reachability notifier")
+        }
     }
     
     
@@ -26,30 +29,23 @@ class ReachabilityManager {
 
 extension ReachabilityManager {
     
-    func startObserving(inViewController vc: UIViewController){
-        NotificationCenter.default.addObserver(self, selector: #selector(reachabilityChanged), name: .reachabilityChanged, object: reachability)
-        do{
-            try reachability.startNotifier()
-        }catch{
-            print("error in ReachabilityManager::startObserving, could not start reachability notifier")
-        }
-        targetViewController = vc
-    }
-    
-    @objc public func reachabilityChanged(note: Notification) {
-        guard let reachability = note.object as? Reachability, let vc = self.targetViewController else { return }
-        
-        switch reachability.connection {
-        case .wifi:
-            print("Reachable via WiFi")
+    func startObserving(completionHandler: @escaping(Bool) -> Void){
+        NotificationCenter.default.addObserver(forName: .reachabilityChanged, object: reachability, queue: nil) { notification in
             
-        case .cellular:
-            print("Reachable via Cellular")
+            guard let reachability = notification.object as? Reachability else { return }
+            switch reachability.connection {
+            case .wifi:
+                print("ReachabilityManager: Reachable via WiFi")
+                completionHandler(true)
+                
+            case .cellular:
+                print("ReachabilityManager: Reachable via Cellular")
+                completionHandler(true)
 
-        case .none:
-            let msg = "⚠️您的网络不可用，为了更准确即时地更新您的数据信息，请确保手机能使用WiFi或流量数据。对此给您带来的不便只好忍忍了，反正您也不能来打我。"
-            vc.displayAlert(title: "无法链接到服务器", message: msg, action: "来人！给我拿下！")
-            print("Network not reachable")
+            case .none:
+                print("ReachabilityManager: Network not reachable")
+                completionHandler(false)
+            }
         }
     }
     
