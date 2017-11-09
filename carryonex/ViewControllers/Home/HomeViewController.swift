f79979db1da7cebdd11ee5474d132de3e255f47c
Zian Chen <zian.chen@viacom.com>
2017-11-09 11:37:17 -0500

    Adding storyboard

---
+++ carryonex/ViewControllers/Home/HomeViewController.swift
@@ -0,0 +1,23 @@
+//
+//  HomeViewController.swift
+//  carryonex
+//
+//  Created by Chen, Zian on 11/9/17.
+//  Copyright © 2017 Xin Zou. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
