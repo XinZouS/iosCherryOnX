974eafbe36a6d93bde23a0bdae3c84a3063c577c
[username] <781281662@qq.com>
2017-11-13 17:03:10 -0500

    modify the home Page

---
+++ carryonex/ViewControllers/Home/UserProfileController.swift
@@ -0,0 +1,37 @@
+//
+//  UserProfileController.swift
+//  carryonex
+//
+//  Created by zxbMacPro on 2017/11/13.
+//  Copyright © 2017年 CarryonEx. All rights reserved.
+//
+
+import UIKit
+import AlamofireImage
+
+class UserProfileController: UIViewController {
+    
+    @IBOutlet weak var helloLabel: UILabel!
+    @IBOutlet weak var userProfileImageBtn: UIButton!
+    @IBOutlet weak var senderButton: UIButton!
+    @IBOutlet weak var shiperButton: UIButton!
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    override func viewDidAppear(_ animated: Bool) {
+        loadUserProfile()
+    }
+    
+    private func loadUserProfile(){
+        guard let currUser = ProfileManager.shared.getCurrentUser() else { return }
+        let imgUrl = URL(string: currUser.imageUrl ?? "")
+        URLCache.shared.removeAllCachedResponses()
+        userProfileImageBtn.af_setImage(for: .normal, url: imgUrl!, placeholderImage: #imageLiteral(resourceName: "carryonex_UserInfo"), filter: nil, progress: nil, completion: nil)
+    }
+    
+    @IBAction func userProfileImageBtnTapped(_ sender: Any) {
+    }
+    @IBAction func shiperButtonTapped(_ sender: Any) {
+    }
+    @IBAction func senderButtonTapped(_ sender: Any) {
+    }
+}
---
+++ carryonex/ViewControllers/Home/UserRecentInfoController.swift
@@ -0,0 +1,24 @@
+//
+//  UserRecentInfoController.swift
+//  carryonex
+//
+//  Created by zxbMacPro on 2017/11/13.
+//  Copyright © 2017年 CarryonEx. All rights reserved.
+//
+
+import UIKit
+
+class UserRecentInfoController: UIViewController{
+    
+    @IBOutlet weak var generalCommentBtn: UIButton!
+    override func viewDidLoad() {
+        setupStarView()
+        setupGeneralCommentText()
+    }
+    private func setupGeneralCommentText(){
+    
+    }
+    private func setupStarView(){
+        let creditLevel = generalCommentBtn.titleLabel?.text
+    }
+}
