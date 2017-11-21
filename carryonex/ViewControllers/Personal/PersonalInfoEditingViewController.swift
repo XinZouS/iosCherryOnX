5da30a96c38ccb38ea59bb1503740e8e198e51f9
XinZouS <sanking.saphira@gmail.com>
2017-11-21 11:31:09 -0500

    [ADD] 2 files for personal info VCs;

---
+++ carryonex/ViewControllers/Personal/PersonalInfoEditingViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PersonalInfoEditingViewController.swift
+//  carryonex
+//
+//  Created by Xin Zou on 11/21/17.
+//  Copyright © 2017 CarryonEx. All rights reserved.
+//
+
+import UIKit
+
+
+class PersonalInfoEditingViewController: UIViewController {
+    
+    
+    
+    
+    
+    
+}
---
+++ carryonex/ViewControllers/Personal/ShipperInfoViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ShipperInfoViewController.swift
+//  carryonex
+//
+//  Created by Xin Zou on 11/21/17.
+//  Copyright © 2017 CarryonEx. All rights reserved.
+//
+
+import UIKit
+
+
+class ShipperInfoViewController: UIViewController {
+    
+    
+    
+    
+    
+}
