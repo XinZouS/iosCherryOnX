de85302da2ccb263608464b7e275e4b7900634ea
XinZouS <sanking.saphira@Gmail.com>
2017-12-06 21:40:01 -0500

    [ADD] payment menu in RequestDetailVC; (#395)
    * [ADD] payment menu in RequestDetailVC;
    
    * [MDF] checkmark green color;

--- carryonex/GlobalVariable.swift
+++ carryonex/GlobalVariable.swift
@@ -52,6 +52,8 @@ let colorErroRed = #colorLiteral(red: 1, green: 0.5261772685, blue: 0.5414895289
 let colorErrGray = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
 let colorTheamRed = #colorLiteral(red: 1, green: 0.4149920642, blue: 0.4057528675, alpha: 1)
 let colorLineLightGray = #colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1)
+let colorCheckmarkGreen = #colorLiteral(red: 0.2326686382, green: 0.6842908263, blue: 0.2634965777, alpha: 1)
+
 
 // wechat login judge
 var wxloginStatus :String = ""
@@ -79,7 +81,7 @@ protocol PhoneNumberDelegate: class {
 }
 
 enum Payment {
-    case applePay, paypal, creditCard, wechatPay
+    case alipay, applePay, paypal, creditCard, wechatPay
 }
 
 protocol ZipcodeCellDelegate: class {
