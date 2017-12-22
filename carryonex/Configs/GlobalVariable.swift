f20ee2b8f8a8638406d01bbb230b8e08ef5c805e
XinZouS <sanking.saphira@gmail.com>
2017-12-22 12:41:48 -0500

    [MDF] new trip table to scrollView;

--- carryonex/Configs/GlobalVariable.swift
+++ carryonex/Configs/GlobalVariable.swift
@@ -51,6 +51,7 @@ let colorOkgreen = #colorLiteral(red: 0.2061403394, green: 0.6508488059, blue: 0
 let colorErroRed = #colorLiteral(red: 1, green: 0.5261772685, blue: 0.5414895289, alpha: 1)
 let colorErrGray = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
 let colorTheamRed = #colorLiteral(red: 1, green: 0.4189302325, blue: 0.4186580479, alpha: 1)
+let colorButtonGray = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
 let colorLineLightGray = #colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1)
 let colorCheckmarkGreen = #colorLiteral(red: 0.2326686382, green: 0.6842908263, blue: 0.2634965777, alpha: 1)
 let colorTextFieldUnderLineLightGray = #colorLiteral(red: 0.7529411765, green: 0.8190833926, blue: 0.8650510907, alpha: 1)
--- carryonex/ViewControllers/Carrier/TripController.swift
+++ carryonex/ViewControllers/Carrier/TripController.swift
@@ -12,15 +12,64 @@ class TripController: UIViewController{
     
     let segueIdTripComplete = "tripComplete"
     
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var startAddressTitleLabel: UILabel!
+    @IBOutlet weak var startAddressTextField: ThemTextField!
+    @IBOutlet weak var endAddressTitleLabel: UILabel!
+    @IBOutlet weak var endAddressTextField: ThemTextField!
+    @IBOutlet weak var startDateTitleLabel: UILabel!
+    @IBOutlet weak var startDateTextField: ThemTextField!
+    @IBOutlet weak var noteTitleLabel: UILabel!
+    @IBOutlet weak var noteTextView: ThemTextView!
+
     @IBOutlet weak var confirmTripButton: UIButton!
     
     var gradientLayer: CAGradientLayer!
     
+    let timePicker:UIDatePicker = UIDatePicker()
+    var addressArray = [[String: AnyObject]]()
+    
+    var isNoteFilled = false
+    let textViewPlaceholder = "目的地地址详细信息，寄件人需知的注意事项等"
+    
+    //MARK: - 选择的国家索引
+    var startCountryIndex = 0
+    var startStateIndex = 0
+    var startCitiesIndex = 0
+    
+    var endCountryIndex = 0
+    var endStateIndex = 0
+    var endCitiesIndex = 0
+    
+    var indexOfTextField : Int = 0
+    var pickUpDate :Double = 0
+    var endCity: String = ""
+    var endState: String  = ""
+    var endCountry: String = ""
+    var startCity: String = ""
+    var startState: String = ""
+    var startCountry: String = ""
+    
+    lazy var pickerView : UIPickerView = {
+        let p = UIPickerView()
+        p.dataSource = self
+        p.delegate = self
+        p.isHidden = false
+        p.tag = 3
+        return p
+    }()
+    
+    // MARK: - View
     override func viewDidLoad() {
         super.viewDidLoad()
         AnalyticsManager.shared.startTimeTrackingKey(.carrierDetailFillTime)
         setupNavigationBar()
+        setupScrollView()
+        setupTextFields()
         setupcomfirmTripButton()
+        setUpAddressPicker()
+        setupTimePicker()
+        judgeButtonState()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -47,11 +96,42 @@ class TripController: UIViewController{
         navigationController?.navigationBar.shadowImage = UIImage()
     }
     
+    private func setupScrollView(){
+        scrollView.layer.cornerRadius = 10
+        scrollView.layer.masksToBounds = true
+    }
+
+    private func setupTextFields(){
+        textFieldAddToolBar(startAddressTextField, nil)
+        textFieldAddToolBar(endAddressTextField, nil)
+        textFieldAddToolBar(startDateTextField, nil)
+        textFieldAddToolBar(nil, noteTextView)
+    }
+    
     private func setupcomfirmTripButton(){
         confirmTripButton.isEnabled = false
-        confirmTripButton.backgroundColor = #colorLiteral(red: 0.8972528577, green: 0.9214243889, blue: 0.9380477071, alpha: 1)
-        confirmTripButton.setTitleColor(#colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1), for: .normal)
     }
+    
+    private func setUpAddressPicker(){
+        let path = Bundle.main.path(forResource: "address", ofType:"plist")
+        self.addressArray = NSArray(contentsOfFile: path!) as! Array
+    }
+
+    private func setupTimePicker(){
+        let date = Date()
+        let timeFormatter = DateFormatter()
+        timeFormatter.dateFormat = L("carrier.ui.formatter.date") //"yyyy年MM月dd日"
+        let currentDateStr = timeFormatter.string(from: date) as String
+        if let maxDate: Date = Date.getFutureDateFromNow(year: 2) {
+            timePicker.maximumDate = maxDate
+        }
+        timePicker.datePickerMode = UIDatePickerMode.date
+        timePicker.backgroundColor = .white
+        timePicker.addTarget(self, action: #selector(datePickerValueChanged), for: UIControlEvents.valueChanged)
+        startDateTextField.text = currentDateStr
+        pickUpDate = date.timeIntervalSince1970
+    }
+
     
     private func setupBackgroundColor(){
         gradientLayer = CAGradientLayer()
@@ -64,37 +144,33 @@ class TripController: UIViewController{
     
     @IBAction func commitTripInfo(_ sender: Any) {
         let trip = Trip()
-        if let childVC = self.childViewControllers.first as? TripTableController {
-            
-            if childVC.pickUpDate < (Date().timeIntervalSince1970 - 86400) {    //86400 seconds/day
-                self.displayAlert(title: L("carrier.error.title.date"), message: L("carrier.error.message.date"), action: L("action.ok"))
-                return
+        if self.pickUpDate < (Date().timeIntervalSince1970 - 86400) {    //86400 seconds/day
+            self.displayAlert(title: L("carrier.error.title.date"), message: L("carrier.error.message.date"), action: L("action.ok"))
+            return
+        }
+        AnalyticsManager.shared.finishTimeTrackingKey(.carrierDetailFillTime)
+        
+        let days = Date(timeIntervalSince1970: pickUpDate).days(from: Date())
+        AnalyticsManager.shared.track(.carrierPrePublishDay, attributes: ["days": days])
+        
+        trip.endAddress?.state = endState
+        trip.endAddress?.city = endCity
+        trip.endAddress?.country = Country(rawValue: endCountry)
+        trip.startAddress?.state = startState
+        trip.startAddress?.city = startCity
+        trip.startAddress?.country = Country(rawValue: startCountry)
+        trip.pickupDate = pickUpDate
+        trip.note = noteTextView.text
+    
+        ApiServers.shared.postTripInfo(trip: trip) { (success, msg, tripCode) in
+            if success, let tripCode = tripCode {
+                trip.tripCode = tripCode
+                self.performSegue(withIdentifier: self.segueIdTripComplete, sender: trip)
+                ProfileManager.shared.loadLocalUser(completion: nil)
+                TripOrderDataStore.shared.pull(category: .carrier, delay: 1, completion: nil)
+            } else {
+                print(msg ?? "Failed post trip")
             }
-            AnalyticsManager.shared.finishTimeTrackingKey(.carrierDetailFillTime)
-            
-            let days = Date(timeIntervalSince1970: childVC.pickUpDate).days(from: Date())
-            AnalyticsManager.shared.track(.carrierPrePublishDay, attributes: ["days": days])
-            
-            trip.endAddress?.state = childVC.endState
-            trip.endAddress?.city = childVC.endCity
-            trip.endAddress?.country = Country(rawValue:childVC.endCountry)
-            trip.startAddress?.state = childVC.startState
-            trip.startAddress?.city = childVC.startCity
-            trip.startAddress?.country = Country(rawValue: childVC.startCountry)
-            trip.pickupDate = childVC.pickUpDate
-            trip.note = childVC.otherTextField.text
-            ApiServers.shared.postTripInfo(trip: trip) { (success, msg, tripCode) in
-                if success, let tripCode = tripCode {
-                    trip.tripCode = tripCode
-                    self.performSegue(withIdentifier: self.segueIdTripComplete, sender: trip)
-                    ProfileManager.shared.loadLocalUser(completion: nil)
-                    TripOrderDataStore.shared.pull(category: .carrier, delay: 1, completion: nil)
-                } else {
-                    print(msg ?? "Failed post trip")
-                }
-            }
-        }else{
-            print("something wrong!")
         }
     }
     
@@ -109,3 +185,280 @@ class TripController: UIViewController{
         self.view.layer.insertSublayer(gradientLayer, at: 0)
     }
 }
+
+// MARK: - TextField Delegate
+extension TripController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        dismissKeyboard()
+        return false
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField == startAddressTextField {
+            indexOfTextField = 0
+            pickerView.selectRow(startCountryIndex, inComponent: 0, animated: false)
+            pickerView.selectRow(startStateIndex, inComponent: 1, animated: false)
+            pickerView.selectRow(startCitiesIndex, inComponent: 2, animated: false)
+            textField.inputView = pickerView
+            
+        }else if textField == endAddressTextField {
+            indexOfTextField = 1
+            pickerView.selectRow(endCountryIndex, inComponent: 0, animated: false)
+            pickerView.selectRow(endStateIndex, inComponent: 1, animated: false)
+            pickerView.selectRow(endCitiesIndex, inComponent: 2, animated: false)
+            textField.inputView = pickerView
+
+        }else if textField == startDateTextField {
+            indexOfTextField = 2
+            textField.inputView = timePicker
+
+        }
+    }
+    
+    fileprivate func textFieldAddToolBar(_ textField: UITextField?, _ textView: UITextView?) {
+        let bar = UIToolbar()
+        bar.barStyle = .default
+        bar.isTranslucent = true
+        bar.tintColor = .black
+        
+        let doneBtn = UIBarButtonItem(title: L("action.done"), style: .done, target: self, action: #selector(textFieldDoneButtonTapped))
+        let cancelBtn = UIBarButtonItem(title: L("action.cancel"), style: .plain, target: self, action: #selector(textFieldCancelButtonTapped))
+        let spaceBtn = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        bar.setItems([cancelBtn, spaceBtn, doneBtn], animated: false)
+        bar.isUserInteractionEnabled = true
+        bar.sizeToFit()
+        
+        if let tf = textField {
+            tf.delegate = self
+            tf.inputAccessoryView = bar
+        }
+        if let tv = textView {
+            tv.delegate = self
+            tv.inputAccessoryView = bar
+            tv.textColor = .lightGray
+            tv.text = textViewPlaceholder
+        }
+    }
+    func textFieldDoneButtonTapped(){
+        dismissKeyboard()
+        switch indexOfTextField {
+        case 0:
+            let location = locationTuples(countryIdx: startCountryIndex, stateIdx: startStateIndex, cityIdx: startCitiesIndex)
+            startCountry = location.0
+            startState = location.1
+            startCity = location.2
+            self.startAddressTextField.text = startCountry + " " + startState + " " + startCity
+            
+        case 1:
+            let location = locationTuples(countryIdx: endCountryIndex, stateIdx: endStateIndex, cityIdx: endCitiesIndex)
+            endCountry = location.0
+            endState = location.1
+            endCity = location.2
+            self.endAddressTextField.text = endCountry + " " + endState + " " + endCity
+            
+        default:
+            print("no picker")
+        }
+        judgeButtonState()
+    }
+    func textFieldCancelButtonTapped(){
+        noteTextView.resignFirstResponder()
+    }
+    
+    fileprivate func dismissKeyboard(){
+        startAddressTextField.resignFirstResponder()
+        endAddressTextField.resignFirstResponder()
+        startDateTextField.resignFirstResponder()
+        noteTextView.resignFirstResponder()
+    }
+    
+}
+
+// MARK: - TextView Delegate
+extension TripController: UITextViewDelegate {
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        indexOfTextField = 3
+        textView.text = isNoteFilled ? textView.text : ""
+        textView.textColor = .black
+        let textViewLoc = textView.convert(CGPoint(x: 0, y: 0), to: view)
+        if textViewLoc.y - view.frame.height < 210 {
+            scrollView.setContentOffset(CGPoint(x: 0, y: 210), animated: true)
+        }
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        isNoteFilled = !textView.text.isEmpty
+        textView.text = isNoteFilled ? textView.text : textViewPlaceholder
+        textView.textColor = isNoteFilled ? .black : .lightGray
+        scrollView.setContentOffset(CGPoint(x: 0, y: 0), animated: true)
+    }
+    
+}
+
+
+// MARK: - PickerView DataSource
+extension TripController: UIPickerViewDataSource {
+    //设置选择框的行数，继承于UIPickerViewDataSource协议
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        
+        let countryIndex = (indexOfTextField == 0) ? startCountryIndex : endCountryIndex
+        let stateIndex = (indexOfTextField == 0) ? startStateIndex : endStateIndex
+        
+        if component == 0 {
+            return self.addressArray.count
+            
+        } else if component == 1 {
+            if let statesArray = self.countryDictionary(countryIdx: countryIndex)?["states"] as? [[String: Any]] {
+                return statesArray.count
+            }
+            return 0
+            
+        } else {
+            if let cityArray = self.stateDictionary(countryIdx: countryIndex, stateIdx: stateIndex)?["cities"] as? [[String: Any]] {
+                return cityArray.count
+            }
+            return 0
+        }
+    }
+    
+}
+
+// MARK: - PickerView Delegate
+extension TripController: UIPickerViewDelegate {
+    
+    //MARK: Plist data processors
+    func locationTuples(countryIdx: Int, stateIdx: Int, cityIdx: Int) -> (String, String, String) {
+        let country = countryDictionary(countryIdx: countryIdx)
+        let state = stateDictionary(countryIdx: countryIdx, stateIdx: stateIdx)
+        let city = cityDictionary(countryIdx: countryIdx, stateIdx: stateIdx, cityIdx: cityIdx)
+        
+        let countryStr = country!["country"] as! String
+        let stateStr = state!["state"] as! String
+        let cityStr = city!["city"] as! String
+        
+        return (countryStr, stateStr, cityStr)
+    }
+    
+    func countryDictionary(countryIdx: Int) -> [String: Any]? {
+        if countryIdx < self.addressArray.count {
+            return self.addressArray[countryIdx]
+        }
+        return nil
+    }
+    
+    func stateDictionary(countryIdx: Int, stateIdx: Int) -> [String: Any]? {
+        if let country = countryDictionary(countryIdx: countryIdx), let states = country["states"] as? [[String: Any]?] {
+            if states.count > stateIdx {
+                return states[stateIdx]
+            }
+            return nil
+        }
+        return nil
+    }
+    
+    func cityDictionary(countryIdx: Int, stateIdx: Int, cityIdx: Int) -> [String: Any]? {
+        if let state = stateDictionary(countryIdx: countryIdx, stateIdx: stateIdx), let cities = state["cities"] as? [[String: Any]?] {
+            if cities.count > cityIdx {
+                return cities[cityIdx]
+            }
+            return nil
+        }
+        return nil
+    }
+    
+    
+    //设置选择框各选项的内容，继承于UIPickerViewDelegate协议
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        let countryIndex = (indexOfTextField == 0) ? startCountryIndex : endCountryIndex
+        let stateIndex = (indexOfTextField == 0) ? startStateIndex : endStateIndex
+        
+        if component == 0 {
+            if let countryName = countryDictionary(countryIdx: row)?["country"] as? String {
+                return countryName
+            }
+            return ""
+            
+        } else if component == 1 {
+            if let stateName = stateDictionary(countryIdx: countryIndex, stateIdx: row)?["state"] as? String {
+                return stateName
+            }
+            return ""
+            
+        } else {
+            if let cityName = cityDictionary(countryIdx: countryIndex, stateIdx: stateIndex, cityIdx: row)?["city"] as? String {
+                return cityName
+            }
+            return ""
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        //根据列、行索引判断需要改变数据的区域
+        switch (component) {
+        case 0:
+            if indexOfTextField == 0 {
+                startCountryIndex = row
+                startStateIndex = 0
+                startCitiesIndex = 0
+            } else {
+                endCountryIndex = row
+                endStateIndex = 0
+                endCitiesIndex = 0
+            }
+            pickerView.reloadComponent(1)
+            pickerView.reloadComponent(2)
+            pickerView.selectRow(0, inComponent: 1, animated: false)
+            pickerView.selectRow(0, inComponent: 2, animated: false)
+            
+        case 1:
+            if indexOfTextField == 0 {
+                startStateIndex = row
+                startCitiesIndex = 0
+            } else {
+                endStateIndex = row
+                endCitiesIndex = 0
+            }
+            pickerView.reloadComponent(2)
+            pickerView.selectRow(0, inComponent: 2, animated: false)
+            
+        case 2:
+            if indexOfTextField == 0 {
+                startCitiesIndex = row
+            } else {
+                endCitiesIndex = row
+            }
+        default:
+            break;
+        }
+    }
+    
+    //设置选择框的列数为3列,继承于UIPickerViewDataSource协议
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 3
+    }
+
+    @objc fileprivate func datePickerValueChanged(){
+        let formatter = DateFormatter()
+        formatter.dateFormat = L("carrier.ui.formatter.date") // "yyyy年MM月dd日"
+        let date = timePicker.date
+        let dateText = formatter.string(from: date)
+        startDateTextField.text = dateText
+        pickUpDate = date.timeIntervalSince1970
+    }
+    
+    fileprivate func judgeButtonState(){
+        let isOk = (!(startAddressTextField.text?.isEmpty ?? true) &&
+            !(endAddressTextField.text?.isEmpty ?? true) &&
+            !(startDateTextField.text?.isEmpty ?? true))
+
+        confirmTripButton.backgroundColor = isOk ? colorTheamRed : colorButtonGray
+        confirmTripButton.setTitleColor((isOk ? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1) : #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)), for: .normal)
+        confirmTripButton.backgroundColor = isOk ? colorTheamRed : #colorLiteral(red: 0.8972528577, green: 0.9214243889, blue: 0.9380477071, alpha: 1)
+        confirmTripButton.isEnabled = isOk
+    }
+    
+}
+
