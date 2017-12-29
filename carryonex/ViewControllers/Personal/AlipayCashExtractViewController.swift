dcb8546c7ec3ffd7c88ec3997c700dad9bcb8ef9
Zian Chen <doomon@gmail.com>
2017-12-29 00:48:54 -0500

    Payout feature

--- carryonex/ViewControllers/Personal/AlipayCashExtractViewController.swift
+++ carryonex/ViewControllers/Personal/AlipayCashExtractViewController.swift
@@ -17,13 +17,35 @@ class AlipayCashExtract: UIViewController {
     @IBOutlet weak var cashAvailableLabel: UILabel!
     @IBOutlet weak var extractValueButton: UIButton!
     @IBOutlet weak var doneButton: UIButton!
+    var circleIndicator: BPCircleActivityIndicator!
     
     @IBAction func extractValueButtonTapped(_ sender: Any) {
         cashExtract = cashAvailable
     }
     
     @IBAction func doneButtonTapped(_ sender: Any) {
-        // TODO: doneButtonTapped;
+        
+        guard let alipayId = alipayAccountTextField.text else {
+            displayAlert(title: "账户错误", message: "请输入支付宝账号", action: L("action.ok"))
+            return
+        }
+        
+        guard let payoutAmount = cashExtractTextField.text, let payout = Double(payoutAmount), payout > cashExtract else {
+            displayAlert(title: "提现金额错误", message: "请输入正确的提现金额", action:  L("action.ok"))
+            return
+        }
+        
+        self.circleIndicator.isHidden = false
+        self.circleIndicator.animate()
+        ApiServers.shared.postWalletAliPayout(logonId: alipayId, amount: payoutAmount) { (success, error) in
+            //Fix this after server fixed.
+            self.displayAlert(title: "提现成功", message: "我们已经成功转账到您的账户，请查看您的支付宝", action:  L("action.ok"))
+        
+            self.circleIndicator.stop()
+            self.circleIndicator.isHidden = true
+            
+            //Reload data on this page
+        }
     }
     
     
@@ -51,8 +73,16 @@ class AlipayCashExtract: UIViewController {
         
         setupUnderlines()
         setupTextFields()
-        cashExtractTextField.becomeFirstResponder()
+        alipayAccountTextField.becomeFirstResponder()
+        setupActivityIndicator()
         getCashAvailableFromServer()
+    }
+    
+    private func setupActivityIndicator(){
+        circleIndicator = BPCircleActivityIndicator()
+        circleIndicator.isHidden = true
+        circleIndicator.center = self.view.center
+        view.addSubview(circleIndicator)
     }
     
     private func setupUnderlines(){
@@ -73,6 +103,9 @@ class AlipayCashExtract: UIViewController {
         //var input: Double = 233.6666666
         //ApiServers.shared.getUserAccountCashValue()
         //cashAvailable = input.roundUp2Decimal()   //DONT DO THIS
+        
+        //TODO UPDATE THIS
+        cashAvailable = 1000.00
     }
 
     
@@ -136,6 +169,7 @@ extension AlipayCashExtract: UITextFieldDelegate {
     
     public func textFieldDidChange(_ textField: UITextField) {
         // check for extractValue <= cashAvailable:
+        //TODO: Add this check in later.
         guard textField == cashExtractTextField else { return }
         let v: Double = Double(textField.text ?? "0.0") ?? 0.0
         let isAvailable = cashAvailable >= v && v >= 0.00
@@ -146,6 +180,7 @@ extension AlipayCashExtract: UITextFieldDelegate {
 
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         // check for backspace key
+        
         if let char = string.cString(using: String.Encoding.utf8) {
             let isBackSpace = strcmp(char, "\\b")
             if (isBackSpace == -92) {
@@ -162,8 +197,7 @@ extension AlipayCashExtract: UITextFieldDelegate {
         let reportProgress = NSRegularExpression.MatchingOptions.reportProgress
         let regex = try! NSRegularExpression(pattern: expression, options: allowCommentAndWitespace)
         let numberOfMatches = regex.numberOfMatches(in: newString, options: reportProgress, range: NSMakeRange(0, (newString as NSString).length))
-
-        return numberOfMatches != 0
+        return true
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
