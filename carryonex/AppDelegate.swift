caba1d7854d8dad6af5683aba6d7174337b8afb8
Zian Chen <doomon@gmail.com>
2018-01-12 17:45:40 -0500

    Pull profile upon receive of comment

--- carryonex/AppDelegate.swift
+++ carryonex/AppDelegate.swift
@@ -242,6 +242,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                     let message = aps["alert"] as? String {
                     mainViewController.displayGlobalAlert(title: "收到新评价", message: message, action: L("action.ok"), completion: {
                         DeeplinkNavigator.handleDeeplink(deeplink)
+                        ProfileManager.shared.loadLocalUser(completion: nil)
                     })
                 }
             }
